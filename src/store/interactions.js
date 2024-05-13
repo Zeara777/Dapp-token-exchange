c83fe8418d8efe12a3cbe5ba27a2bbbb40e0986a
Rafael Zea <rafael_zea@msn.com>
2024-05-12 19:41:34 -0600

    22. Withdrawals

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -1,6 +1,7 @@
 import { ethers } from 'ethers'
 import TOKEN_ABI from '../abis/Token.json';
 import EXCHANGE_ABI from '../abis/Exchange.json';
+import { extendConfig } from 'hardhat/config';
 
 export const loadProvider = (dispatch) => {
   const connection = new ethers.providers.Web3Provider(window.ethereum)
@@ -55,6 +56,10 @@ export const subscribeToEvents = (exchange, dispatch) => {
   exchange.on('Deposit', (token, user, amount, balance, event) => {
     dispatch({ type: 'TRANSFER_SUCCESS', event })
   })
+
+  exchange.on('Withdraw'), (token, user, amount, balance, event) => {
+    dispatch({ type: 'TRANSFER_SUCCESS', event })
+  }
 }
 
 // ------------------------------------------------------------------------------
@@ -88,9 +93,13 @@ export const transferTokens =  async (provider, exchange, transferType, token, a
     const signer = await provider.getSigner()
     const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18)
 
-    transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
-    await transaction.wait()
-    transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
+    if (transferType === 'Deposit') {
+      transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
+      await transaction.wait()
+      transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
+    } else {
+      transaction = await exchange.connect(signer).withdrawToken(token.address, amountToTransfer)
+    }
 
     await transaction.wait()
 
